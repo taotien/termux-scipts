ca08b3c2f3f1f6e2046ac1a16710f2aad96feb97
Tao Tien <me@taotien.com>
2019-09-20 22:43:56 +0800

    Update

--- croc.sh
+++ croc.sh
@@ -1,7 +1,11 @@
 #!
 
-PAKE=$(termux-dialog -t "croc PAKE" | jq -r .text)
+# Downloads to "~/storage/downloads/croc" with predetermined or user-input code
+
+# Uncomment to get user input dialog
+# PAKE=$(termux-dialog -t "croc PAKE" | jq -r .text)
 
 cd ~/storage/downloads/croc
-croc --yes $PAKE 
+#replace #$PAKE or uncomment
+croc --yes #$PAKE
 exit 0
--- ytdl_mp3-mp4.sh
+++ ytdl_mp3-mp4.sh
@@ -3,7 +3,7 @@
 # Prompts user to youtube-dl a file as MP3 or MP4, then runs youtube-dl using copied link
 # Depends on jq and termux-api. Run "pkg install jq termux-api" . Works with termux version 0.75
 # Setup storage permissions by running "termux-setup-storage"
-# You may want to have FFMPEG available as well: "pkg install ffmpeg
+# You may want to have FFMPEG available as well: "pkg install ffmpeg"
 
 MPX=$(termux-dialog radio -v "MP3,MP4" -t "Download as..." | jq -r .index)
 
