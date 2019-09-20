06968ddaf78931aa401616642874fede8890df90
Tao Tien <me@taotien.com>
2019-09-20 22:54:33 +0800

    Update

--- croc.sh
+++ croc.sh
@@ -1,11 +1,11 @@
 #!
 
-# Downloads to "~/storage/downloads/croc" with predetermined or user-input code. Relies on https://getcroc.schollz.com
+# Downloads to "~/storage/downloads/croc" with predetermined or user-input code. Relies on https://schollz.com/software/croc6
 
 # Uncomment to get user input dialog
-# PAKE=$(termux-dialog -t "croc PAKE" | jq -r .text)
+PAKE=$(termux-dialog -t "croc PAKE" | jq -r .text)
 
 cd ~/storage/downloads/croc
 #replace #$PAKE or uncomment
-croc --yes #$PAKE
+croc --yes $PAKE
 exit 0
