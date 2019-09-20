9df2ee9842cb20ff48e9425edb6cb98adadb4d4c
Tao Tien <me@taotien.com>
2019-09-20 23:45:50 +0800

    Update

--- croc.sh
+++ croc.sh
@@ -2,13 +2,13 @@
 
 # Downloads to "~/storage/downloads/croc" with predetermined or user-input code. Relies on https://schollz.com/software/croc6
 
-# Uncomment to get user input dialog
+# Comment to disable user input dialog
 PAKE=$(termux-dialog -t "croc PAKE" | jq -r .text)
 
 cd ~/storage/downloads/croc
-#replace #$PAKE or uncomment
+# Replace #$PAKE for predetermined code
 croc --yes $PAKE
 
-termux-notification -c "Files croc'd"
+termux-toast -g middle "Files croc'd"
 
 exit 0
