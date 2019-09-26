03b5e0b33cd7f683a95e98960220552bbbc7046a
Tao Tien <me@taotien.com>
2019-09-26 19:27:00 +0800

    Update croc.sh

--- croc.sh
+++ croc.sh
@@ -1,6 +1,6 @@
 #!
 
-# Downloads to "~/storage/downloads/croc" with predetermined or user-input code. Relies on https://schollz.com/software/croc6
+# Downloads to "~/storage/downloads/croc" with user-input code. Relies on https://schollz.com/software/croc6
 
 # Comment to disable user input dialog
 PAKE=$(termux-dialog -t "croc PAKE" | jq -r .text)
